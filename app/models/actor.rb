d38856aad8e562bd3d5937d74d456d5d0b93c389
kimo5532 <shotbyabel@gmail.com>
2015-08-03 15:59:23 -0700

    fixed merge, took out notes

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,5 @@
 class Actor < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-  
   has_and_belongs_to_many :movies
->>>>>>> 5e394cf751f7edff452ced37608d8372344dffb9
+
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,5 @@
 class Movie < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :reviews
-=======
 
   has_and_belongs_to_many :actors
-
->>>>>>> 5e394cf751f7edff452ced37608d8372344dffb9
 end
