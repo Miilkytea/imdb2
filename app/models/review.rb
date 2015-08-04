96cecb763fd5ed22d896e092bd4f16d9c52a1ca4
Tiffany Chang <chang.tiffany@gmail.com>
2015-08-03 20:20:12 -0700

    Fixed merge conflict messages in review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,15 +1,7 @@
 class Review < ActiveRecord::Base
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-  belongs_to :user
-=======
-  
-  belongs_to :movie
->>>>>>> movie_review
-=======
   belongs_to :movie
 
   belongs_to :user
->>>>>>> e4d4061acbb3037ad7b1bb8d8b0f98622aba1c9b
+
 end
