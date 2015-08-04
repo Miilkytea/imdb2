696f174785e2338590733294d41871b2f26c3921
kimo5532 <shotbyabel@gmail.com>
2015-08-03 22:37:09 -0700

    added show to actors resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   resources :movies
-  resources :actors, only: [:index, :new, :create, :edit]
+  resources :actors, only: [:index, :new, :show, :create, :edit]
 end
